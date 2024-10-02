a545a202a11ba41be4a41bb3d3a16674d4b2c68b
Ayush Bhattarai <git@ayushb.com>
2024-10-02 19:36:16 +0545

    Initial Setup

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+: '
+#!/usr/bin/env pwsh
+<#'
+echo "This is a placeholder script for Django setup (Bash version)"
+echo "Running on Linux/macOS"
+exit 0
+#>
+
+Write-Output "This is a placeholder script for Django setup (PowerShell version)"
+Write-Output "Running on Windows"
+exit 0
+
+# Common code (ignored by both Bash and PowerShell)
+# Your actual Django setup script will go here later
